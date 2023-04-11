547a8cd238dd5fe94c35f7504213fa82c7cb67bc
gioche96 <129050252+gioche96@users.noreply.github.com>
2023-04-11 13:55:17 +0200

    Running FastQC

---
+++ 0.fastqc.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+## Required software
+module load fastqc
+
+## Setting the environment
+input="pwd"
+
+## Performing FastQC locally
+for i 
+in ${pwd}*.fastq.gz;
+do sbatch -p medium -C scratch -t 0-01:00:00 -c4 -N 1 --wrap="fastqc ${i} > ${i}_out.txt";
+done
