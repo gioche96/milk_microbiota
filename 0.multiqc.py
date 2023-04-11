c30804bf048b773dc371b53ebb89f0381821c38f
gioche96 <129050252+gioche96@users.noreply.github.com>
2023-04-11 15:48:03 +0200

    Running MultiQC

---
+++ 0.multiqc.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+"""
+Load python3 prior to run:
+> module load python/3
+> ./0.multiqc.py
+"""
+
+## Required software
+import multiqc
+
+## Performing MultiQC locally
+multiqc.run("pwd")		## Change according to the pwd containing the FastQC.zip files
