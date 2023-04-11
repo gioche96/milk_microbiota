b6da088ef0093126e7017c0b6ef6283a0ec5030d
gioche96 <129050252+gioche96@users.noreply.github.com>
2023-04-11 13:53:52 +0200

    Running MultiQC
    Load python/3 prior to run: 
    > module load python/3
    > ./0.multiqc.sh

---
+++ 0.multiqc.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+## Required software
+import multiqc
+
+## Performing MultiQC locally in the FastQC files folder
+multiqc.run("pwd")
+
+print (command)
+
+subprocess.call([command], shell=True)
+
