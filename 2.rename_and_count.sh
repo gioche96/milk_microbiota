a8111690fa3370ef425623c63977bc2a45ff00d7
gioche96 <129050252+gioche96@users.noreply.github.com>
2023-04-12 14:12:05 +0200

    Renaming sample names and Counting reads

---
+++ 2.rename_and_count.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+
+##########################################################################
+##			Script to rename samples and count the reads		##
+##########################################################################
+
+## Setting the environmnent
+project_home=$(pwd)		## This is supposed to be the main folder
+datapath="$(pwd)/0_Original_files"    ## Change according to the original files folder
+outdir="$(pwd)/2_Renamed_and_count"
+core=8
+
+## Make folder
+cd $project_home
+
+if [ ! -d $outdir ]; then
+	mkdir -p $outdir
+fi
+
+## Rename samples
+cd $datapath
+
+for i in *.fastq.gz
+do
+  exte=".fastq.gz" ## Final extension
+  sample=$(echo "$i" | cut -d "_" -f2) ## It depends on the structure of the name
+  read=$(echo "$i" | cut -d "_" -f5) ## It depends on the structure of the name
+  cp $i ${outdir}/$sample"_"$read$exte
+  echo -e "$i\t-->\t$sample"_"$read$exte" >> ${outdir}/Renamed_samples.txt
+done
+
+## Count reads
+cd ${outdir}
+
+for i in *.fastq.gz
+do
+        echo -n $i >> Seq_count_16S_raw.txt
+        echo -n " = " >> Seq_count_16S_raw.txt
+        echo $(zcat $i | wc -l) / 4 | bc >> Seq_count_16S_raw.txt   ## The sum is divided by 4 because for each sequence there are 4 lines of information
+done
+
+echo "DONE!"
+
+##########################################################################
