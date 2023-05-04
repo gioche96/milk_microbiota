a9572513c2f20c115fa09e5a00f50f51a3451bb7
gioche96 <129050252+gioche96@users.noreply.github.com>
2023-05-04 14:38:49 +0200

    Quality check

--- 1.quality_check.sh
+++ 1.quality_check.sh
@@ -31,7 +31,7 @@ echo "running MultiQC from locally installed software"
 multiqc .
 
 ## Give permissions to subfolders and files
-chmod -R g+rwx /scratch/users/gchessa/PROVA_Microbiota2/*  ## Main folder
+chmod -R g+rwx /scratch/users/gchessa/PROVA_Microbiota/*  ## Main folder
 
 echo "Done"
 
